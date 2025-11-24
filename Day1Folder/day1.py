5d32c8f3f07f10afba24d6ca5bc97a3c9ceae769
computhink-jonathon <JONATHON.LEE@COMPUTHINK.COM.SG>
2025-11-24 08:33:50 +0000

    Nov 24, 2025, 8:33 AM

--- Day1Folder/day1.py
+++ Day1Folder/day1.py
@@ -1,6 +1,7 @@
 # Write all your codes for Day 1 here.
 # COMMENT out the previous task before going on to the next task
 print("hello from day1")
+print("hello yo")
 
 ########################################################################
 # Task 1:
@@ -9,22 +10,37 @@ print("hello from day1")
 
 ########################################################################
 # Task 2:
-
+print("Jonathon")
+print("how are you")
+print("what school do you attend")
+print("")
 
 
 ########################################################################
 # Task 3:
+animal1 = "Tiger"
+animal2 = "Dragon"
+animal3 = "Phoneix"
+print(animal1)
+print(animal2)
+print(animal3)
 
 
 
 ########################################################################
 # Task 4:
+ans = 1+1
+print(ans)
 
 
 
 ########################################################################
 # Task 5:
+name = "Jonathon"
+food = "chicken rice"
 
+print("My name is " + name)
+print("My name is" + name + " Lee")
 
 
 ########################################################################
@@ -34,6 +50,10 @@ print("hello from day1")
 
 ########################################################################
 # Task 7:
+def addition(number1, number2):
+    print(number1 + number2)
+
+addition(3,8)
 
 
 
