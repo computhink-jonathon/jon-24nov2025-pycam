c82729a3b42ab86383284dd498413d7fbcf4a04e
computhink-jonathon <JONATHON.LEE@COMPUTHINK.COM.SG>
2025-11-26 07:01:52 +0000

    Nov 26, 2025, 7:01 AM

--- Day3Folder/day3.py
+++ Day3Folder/day3.py
@@ -46,7 +46,7 @@ print("The division for " + num1 + " and " + num2 + " = " + divide)
 ########################################################################
 # Task 4:
 item = input("What items are you buying?")
-
+unit = input("How many units are you buying")
 
 
 ########################################################################
